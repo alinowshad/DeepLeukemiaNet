fae8eca038e5bd06d511233e51f3fe733bfba30c
Ali Noshad <37798588+alinowshad@users.noreply.github.com>
2021-08-29 17:50:10 +0430

    Update C-NMC-Experiments.py

--- C-NMC/C-NMC-Experiments.py
+++ C-NMC/C-NMC-Experiments.py
@@ -289,28 +289,6 @@ x_train, x_validate, y_train, y_validate = train_test_split(x_train, y_train, te
 x_train.shape
 
 
-# In[31]:
-
-
-#With data augmentation to prevent overfitting and handling the imbalance in dataset
-
-datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range = 30,  # randomly rotate images in the range (degrees, 0 to 180)
-        zoom_range = 0.2, # Randomly zoom image 
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        horizontal_flip = True,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
-
-
-datagen.fit(x_train)
-
-
 # In[32]:
 
 
