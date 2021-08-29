a95c0d72a271212510417df59096362890e1b29d
Ali Noshad <37798588+alinowshad@users.noreply.github.com>
2021-08-29 17:51:03 +0430

    Update ALL-IDB - Experiments.py

--- ALL-IDB/ALL-IDB - Experiments.py
+++ ALL-IDB/ALL-IDB - Experiments.py
@@ -308,28 +308,6 @@ HealthyPaths = []
 NotHealthyPaths = []
 
 
-# In[33]:
-
-
-#With data augmentation to prevent overfitting and handling the imbalance in dataset
-
-datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range = 30,  # randomly rotate images in the range (degrees, 0 to 180)
-        zoom_range = 0.2, # Randomly zoom image 
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        horizontal_flip = True,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
-
-
-datagen.fit(x2_train)
-
-
 # In[34]:
 
 
